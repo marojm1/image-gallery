86f9171814e5f0538bf14b5547d3f1aef70717fc
marojm1 <marojm1@student.op.ac.nz>
2024-08-23 14:48:21 +1200

    images

--- gallery.js
+++ gallery.js
@@ -5,14 +5,24 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
+var filenames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 /* Declaring the alternative text for each image file */
-
+const altTexts = {
+    "pic1.jpg": "Closeup of a blue human eye", 
+    "pic2.jpg":"Ceiling of cave", 
+    "pic3.jpg":"Flowers", 
+   "pic4.jpg":"Egyptian Pharaoh", "Butterfly on a leaf" };
 /* Looping through images */
+for (const filename in filenames)
+{
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${filename}`);
+    newImage.setAttribute('alt', filename);
+    thumbBar.appendChild(newImage);
+    
+}
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+
+
 
 /* Wiring up the Darken/Lighten button */
