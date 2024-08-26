e101587c6d85db5e45389f51a9d99f814207a361
marojm1 <marojm1@student.op.ac.nz>
2024-08-27 11:18:00 +1200

    images are responsive

--- gallery.js
+++ gallery.js
@@ -1,28 +1,43 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
 
 /* Declaring the array of image filenames */
-var filenames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 /* Declaring the alternative text for each image file */
-const altTexts = {
-    "pic1.jpg": "Closeup of a blue human eye", 
-    "pic2.jpg":"Ceiling of cave", 
-    "pic3.jpg":"Flowers", 
-   "pic4.jpg":"Egyptian Pharaoh", "Butterfly on a leaf" };
-/* Looping through images */
-for (const filename in filenames)
-{
-    const newImage = document.createElement('img');
-    newImage.setAttribute('src', `images/${filename}`);
-    newImage.setAttribute('alt', filename);
-    thumbBar.appendChild(newImage);
-    
+
+const alts = {
+  'pic1.jpg' : 'Closeup of a human eye',
+  'pic2.jpg' : 'Rock that looks like a wave',
+  'pic3.jpg' : 'Purple and white pansies',
+  'pic4.jpg' : 'Section of wall from a Pharoah s tomb',
+  'pic5.jpg' : 'Large moth on a leaf'
 }
-
-
-
-
+/* Declaring the array of image filenames */
+for (const image of images) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('alt', alts[image]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', e => {
+      displayedImage.src = e.target.src;
+      displayedImage.alt = e.target.alt;
+    });
+  }
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', () => {
+    const btnClass = btn.getAttribute('class');
+    if (btnClass === 'dark') {
+      btn.setAttribute('class','light');
+      btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+      btn.setAttribute('class','dark');
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+      
+    }
+  });
+  
